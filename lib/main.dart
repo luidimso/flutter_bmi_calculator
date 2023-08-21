1b041828d092ce67bc636c73341717062149f870
luidimso <luidi_mso@outlook.com>
2023-08-21 04:28:25 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("BMI Calculator"),
+        centerTitle: true,
+        backgroundColor: Colors.green,
+        actions: const [
+           IconButton(
+              onPressed: null,
+              icon: Icon(Icons.refresh)
+          )
+        ],
+      ),
+    );
+  }
+}
