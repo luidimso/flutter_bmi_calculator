5d6da90eab036866329de0ad4749f688613bcd27
luidimso <luidi_mso@outlook.com>
2023-08-21 10:00:18 -0300

    Adding form and validators

--- lib/main.dart
+++ lib/main.dart
@@ -19,11 +19,14 @@ class _HomeState extends State<Home> {
   TextEditingController heightController = TextEditingController();
   String infoText = "Insert your data!";
 
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
   void resetFields() {
     setState(() {
       weightController.text = "";
       heightController.text = "";
       infoText = "Insert your data!";
+      formKey = GlobalKey<FormState>();
     });
   }
 
@@ -60,74 +63,91 @@ class _HomeState extends State<Home> {
         ],
       ),
       backgroundColor: Colors.white,
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            const Icon(Icons.person_outline,
-              size: 120.0,
-              color: Colors.green,
-            ),
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: const InputDecoration(
-                labelText: "Weight",
-                labelStyle: TextStyle(
-                  color: Colors.green
-                ),
-              ),
-              controller: weightController,
-              textAlign: TextAlign.center,
-              style: const TextStyle(
+      body: Form(
+        key: formKey,
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const Icon(Icons.person_outline,
+                size: 120.0,
                 color: Colors.green,
-                fontSize: 25.0
               ),
-            ),
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: const InputDecoration(
-                  labelText: "Height",
+              TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(
+                  labelText: "Weight",
                   labelStyle: TextStyle(
-                      color: Colors.green
-                  )
-              ),
-              controller: heightController,
-              textAlign: TextAlign.center,
-              style: const TextStyle(
+                    color: Colors.green
+                  ),
+                ),
+                controller: weightController,
+                validator: (value) {
+                  if(value!.isEmpty){
+                    return "Insert your weight!";
+                  }
+                },
+                textAlign: TextAlign.center,
+                style: const TextStyle(
                   color: Colors.green,
                   fontSize: 25.0
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(
-                top: 10.0,
-                bottom: 10.0
+              TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(
+                    labelText: "Height",
+                    labelStyle: TextStyle(
+                        color: Colors.green
+                    )
+                ),
+                controller: heightController,
+                validator: (value) {
+                  if(value!.isEmpty){
+                    return "Insert your height!";
+                  }
+                },
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                    color: Colors.green,
+                    fontSize: 25.0
+                ),
               ),
-              child: Container(
-                height: 50.0,
-                child: ElevatedButton(
-                  onPressed: calculate,
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all<Color>(Colors.green)
-                  ),
-                  child: const Text("Calculate",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 25.0
+              Padding(
+                padding: const EdgeInsets.only(
+                  top: 10.0,
+                  bottom: 10.0
+                ),
+                child: Container(
+                  height: 50.0,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if(formKey.currentState!.validate()) {
+                        calculate();
+                      }
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(Colors.green)
+                    ),
+                    child: const Text("Calculate",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25.0
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-            Text(infoText,
-              textAlign: TextAlign.center,
-              style: const TextStyle(
-                color: Colors.green,
-                fontSize: 25.0
-              ),
-            )
-          ],
+              Text(infoText,
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  color: Colors.green,
+                  fontSize: 25.0
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
