d145c0c2a977fc4e7c11ee227d2169fde5d75146
luidimso <luidi_mso@outlook.com>
2023-08-21 04:58:30 -0300

    Adding elevated button

--- lib/main.dart
+++ lib/main.dart
@@ -29,42 +29,73 @@ class _HomeState extends State<Home> {
         ],
       ),
       backgroundColor: Colors.white,
-      body: const Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Icon(Icons.person_outline,
-            size: 120.0,
-            color: Colors.green,
-          ),
-          TextField(
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-              labelText: "Weight",
-              labelStyle: TextStyle(
-                color: Colors.green
-              )
-            ),
-            textAlign: TextAlign.center,
-            style: TextStyle(
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const Icon(Icons.person_outline,
+              size: 120.0,
               color: Colors.green,
-              fontSize: 25.0
             ),
-          ),
-          TextField(
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-                labelText: "Height",
+            const TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: "Weight",
                 labelStyle: TextStyle(
-                    color: Colors.green
+                  color: Colors.green
                 )
-            ),
-            textAlign: TextAlign.center,
-            style: TextStyle(
+              ),
+              textAlign: TextAlign.center,
+              style: TextStyle(
                 color: Colors.green,
                 fontSize: 25.0
+              ),
             ),
-          )
-        ],
+            const TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                  labelText: "Height",
+                  labelStyle: TextStyle(
+                      color: Colors.green
+                  )
+              ),
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  color: Colors.green,
+                  fontSize: 25.0
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                top: 10.0,
+                bottom: 10.0
+              ),
+              child: Container(
+                height: 50.0,
+                child: ElevatedButton(
+                  onPressed: null,
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(Colors.green)
+                  ),
+                  child: const Text("Calculate",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 25.0
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const Text("Test",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.green,
+                fontSize: 25.0
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
